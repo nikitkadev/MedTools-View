8ddff78062d754f52e5e0ce27002f02eea295b11
nikitkadev <nekitosik.pav@gmail.com>
2026-08-20 10:58:50 +0700

    fix: расширить каскадный сброс состояний

--- src/modules/rControl/widgets/workspace/model/store/useWorkspaceStore.ts
+++ src/modules/rControl/widgets/workspace/model/store/useWorkspaceStore.ts
@@ -7,15 +7,17 @@ interface WorkspaceStore {
   selectedCompletedCaseUid: number | null;
   selectedMedicalCaseUid: number | null;
   selectedProvidedServiceUid: number | null;
+  selectedOncologyServiceUid: number | null;
+  selectedMedicationUid: number | null;
+
   invoicesTablePagination: PaginationState;
   completedCasesTablePagination: PaginationState;
   defectsTablePagination: PaginationState;
+
   targetCategory: CategoryId;
-  selectedOncologyServiceUid: number | null;
-  selectedMedicationUid: number | null;
+
   selectOncologyService: (oncologyServiceUid: number | null) => void;
   selectMedication: (medicationUid: number | null) => void;
-  setTargetCategory: (targetCategory: CategoryId) => void;
   selectInvoice: (invoiceUid: number | null) => void;
   selectCompletedCase: (completedCaseUid: number | null) => void;
   selectMedicalCase: (medicalCaseUid: number | null) => void;
@@ -29,6 +31,7 @@ interface WorkspaceStore {
   setDefectsTablePagination: (
     newState: Partial<WorkspaceStore["defectsTablePagination"]>,
   ) => void;
+  setTargetCategory: (targetCategory: CategoryId) => void;
 }
 
 export const useWorkspaceStore = create<WorkspaceStore>((set) => ({
@@ -36,22 +39,9 @@ export const useWorkspaceStore = create<WorkspaceStore>((set) => ({
   selectedCompletedCaseUid: null,
   selectedMedicalCaseUid: null,
   selectedProvidedServiceUid: null,
-  targetCategory: "default",
   selectedOncologyServiceUid: null,
   selectedMedicationUid: null,
-  selectOncologyService: (oncologyServiceUid) =>
-    set({
-      selectedOncologyServiceUid: oncologyServiceUid,
-      selectedMedicationUid: null,
-    }),
-  selectMedication: (medicationUid) =>
-    set({
-      selectedMedicationUid: medicationUid,
-    }),
-  setTargetCategory: (targetCategory) =>
-    set({
-      targetCategory: targetCategory,
-    }),
+
   invoicesTablePagination: {
     page: 0,
     pageSize: 10,
@@ -64,11 +54,21 @@ export const useWorkspaceStore = create<WorkspaceStore>((set) => ({
     page: 0,
     pageSize: 10,
   },
+
+  targetCategory: "default",
+
+  setTargetCategory: (targetCategory) =>
+    set({
+      targetCategory: targetCategory,
+    }),
   selectInvoice: (invoiceUid) =>
     set((state) => ({
       selectedInvoiceUid: invoiceUid,
       selectedCompletedCaseUid: null,
+      selectedMedicalCaseUid: null,
       selectedProvidedServiceUid: null,
+      selectedOncologyServiceUid: null,
+      selectedMedicationUid: null,
       completedCasesTablePagination: {
         ...state.completedCasesTablePagination,
         page: 0,
@@ -79,15 +79,28 @@ export const useWorkspaceStore = create<WorkspaceStore>((set) => ({
       selectedCompletedCaseUid: completedCaseUid,
       selectedMedicalCaseUid: null,
       selectedProvidedServiceUid: null,
+      selectedOncologyServiceUid: null,
+      selectedMedicationUid: null,
     }),
   selectMedicalCase: (medicalCaseUid) =>
     set({
       selectedMedicalCaseUid: medicalCaseUid,
       selectedProvidedServiceUid: null,
+      selectedOncologyServiceUid: null,
+      selectedMedicationUid: null,
     }),
   selectProvidedService: (providedServiceUid) =>
     set({
       selectedProvidedServiceUid: providedServiceUid,
+    }),
+  selectOncologyService: (oncologyServiceUid) =>
+    set({
+      selectedOncologyServiceUid: oncologyServiceUid,
+      selectedMedicationUid: null,
+    }),
+  selectMedication: (medicationUid) =>
+    set({
+      selectedMedicationUid: medicationUid,
     }),
   setInvoicesTablePagination: (newState) =>
     set((state) => ({
